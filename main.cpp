c25f47778adce6fdec6135a27f2ed0e4d697d5f9
Lawrence Patrick Calulo <lugkhast@gmail.com>
2015-10-17 15:26:37 +0800

    Add config header file

--- main.cpp
+++ main.cpp
@@ -1,25 +1,16 @@
 #include <iostream>
 
+#include "GameConfig.h"
+
 #include "MovementHandler.h"
 #include "Movable.h"
 #include "SDLApp.h"
 
 int main(int argc, char **argv)
 {
-    std::cout << "Hello, world!" << std::endl;
-
-    Movable *m1, *m2;
-    m1 = new Movable();
-    m2 = new Movable();
-
-    MovementHandler mh;
-    mh.put(m1);
-    mh.put(m2);
-
-    mh.printMovableCount();
-    mh.tick();
-
-    std::cout << m1->getCoordString() << std::endl;
+    std::cout << "walksim "
+        << GAME_VERSION_MAJOR << "." << GAME_VERSION_MINOR
+        << std::endl;
 
     SDLApp app;
     app.start();
