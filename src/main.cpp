6931cf79a187f8b0d1c409a6a828376c981373ac
Lawrence Patrick Calulo <lugkhast@gmail.com>
2015-10-18 15:35:48 +0800

    Catch lsException in main() for error output

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "GameConfig.h"
 #include "SDLApp.h"
+#include "lugSDL/lsException.h"
 
 int main(int argc, char **argv)
 {
@@ -9,8 +10,15 @@ int main(int argc, char **argv)
         << GAME_VERSION_MAJOR << "." << GAME_VERSION_MINOR
         << std::endl;
 
-    SDLApp app;
-    app.start();
+    SDLApp *app = NULL;
+
+    try {
+        app = new SDLApp;
+        app->start();
+    } catch (lsException ex) {
+        std::cout << "Caught exception, exiting: " << std::endl
+            << ex.message << std::endl;
+    }
 
     return 0;
 }
